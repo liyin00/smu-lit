10a5423f10f527de92d85cdd72157e3ead7719c4
alimsihui <71798618+alimsihui@users.noreply.github.com>
2022-06-22 13:30:11 +0800

    Homepage + stylesheet

---
+++ Homepage.php
@@ -0,0 +1,18 @@
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Gothic+A1">
+    <style>
+      p {
+        font-family: 'Gothic A1', serif;
+      }
+    </style>
+    
+  </head>
+  <body>
+    <div>
+        <p>Lorem ipsum</p>
+    </div>
+  </body>
+</html>
